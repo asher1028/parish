cd13aaaaf32cc803106eb38873c7e3b69095b5bd
asher1028 <youremail@example.com>
2024-12-01 15:00:45 +0800

    updates

--- config/connection.php
+++ config/connection.php
@@ -5,10 +5,10 @@ $password = "P@55w0rd1982"; // Database password
 $dbname = "u868004123_parishv2"; // Database name
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-if ($conn->connect_error) {
+// if ($conn->connect_error) {
 //   die("Connection failed: " . $conn->connect_error);
-}
+// }
 ?>
