c4b9f706b09f778ceb5e9466497655f2463a2f5e
asher1028 <youremail@example.com>
2024-12-01 14:53:57 +0800

    updates

--- config/connection.php
+++ config/connection.php
@@ -1,14 +1,14 @@
 <?php
 $servername = "localhost"; // Change if needed
-$username = "root"; // Database username
-$password = ""; // Database password
-$dbname = "parish"; // Database name
+$username = "u868004123_parish2"; // Database username
+$password = "P@55w0rd1982"; // Database password
+$dbname = "u868004123_parishv2"; // Database name
 
 // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-// if ($conn->connect_error) {
-//    die("Connection failed: " . $conn->connect_error);
-//}
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+}
 ?>
