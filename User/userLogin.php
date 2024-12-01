76d26b134f97b28536d3f92463a9db89eda9d37e
asher1028 <youremail@example.com>
2024-12-01 15:34:57 +0800

    a

--- User/userLogin.php
+++ User/userLogin.php
@@ -67,7 +67,7 @@ if (isset($_SESSION['username'])) {
 <html>
 <head>
     <title>Parish of San Juan</title>
-    <link rel="stylesheet" href="UserLogin.css">
+    <link rel="stylesheet" href="userLogin.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <style>
         .loginLinks {
